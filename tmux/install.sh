42cb29f5a5345810f84ee5dd46f4f5ef2b3417bb
Philipp Benz <phibenz@gmail.com>
2025-08-22 00:43:51 -0700

    feat: move path display from tmux to starship, add gitmux integration

--- tmux/install.sh
+++ tmux/install.sh
@@ -17,10 +17,11 @@ fi
 ln -sf "$(pwd)/tmux.conf" ~/.tmux.conf
 
 # Install TPM
-if [ -d ~/.tmux/plugins/tpm ]; then
-    cd ~/.tmux/plugins/tpm && git pull
+mkdir -p ~/.config/tmux/plugins
+if [ -d ~/.config/tmux/plugins/tpm ]; then
+    cd ~/.config/tmux/plugins/tpm && git pull
 else
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 fi
 
 # Install Catppuccin theme
@@ -30,5 +31,18 @@ if [ -d ~/.config/tmux/plugins/catppuccin/tmux ]; then
 else
     git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 fi
+
+# Install gitmux
+if ! command -v gitmux &> /dev/null; then
+    if command -v brew &> /dev/null; then
+        brew install gitmux
+    else
+        echo "WARNING: gitmux not found and brew not available"
+    fi
+fi
+
+# Setup gitmux config
+mkdir -p ~/.config/tmux
+ln -sf "$(realpath .gitmux.yml)" ~/.config/tmux/gitmux.yml
 
 echo "Done. Press Ctrl-a + I in tmux to install plugins."
